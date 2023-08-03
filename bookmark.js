95c939acacd509a280ea20cab35859333b987d29
Lemon <70281701+Lenn106@users.noreply.github.com>
2023-08-02 19:53:32 -0500

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-//Due to CA problems, the hacks will only be avaliable in our discord server. https://dsc.gg/neomenu
+//Due to CA problems, the latest hacks will only be avaliable in our discord server. Otherwise, you can use these with possible issues. https://dsc.gg/neomenu
